95d5b47fe0374d5a3dfc7603a8b3eb6e75fbc7fc
tlichtenegger <thomas.lichtenegger@jku.at>
2025-02-20 14:49:55 +0100

    Create additional surface files for left and right half of inlet for the usage in DEM simulations as bottom.

--- Fluid/createMesh.sh
+++ Fluid/createMesh.sh
@@ -4,5 +4,10 @@ rm -r constant/polyMesh
 rm -r surface
 mkdir surface
 blockMesh
+
+# extract surface for usage as walls in DEM simulation
 foamToSurface surface/surface.stl
 surfaceSplitByPatch surface/surface.stl
+# split inlet patch into two parts to create outlet for DEM particles
+surfaceSubset surface/surface_in.stl surface/surface_in1.stl system/surfaceSubsetDict1
+surfaceSubset surface/surface_in.stl surface/surface_in2.stl system/surfaceSubsetDict2
